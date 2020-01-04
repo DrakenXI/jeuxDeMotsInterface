032d87ff42ac6db032d5025aeef9c467f361eda8
Amandine Paillard <amandine.paillard@hotmail.fr>
2020-01-04 19:47:12 +0100

    architecture symfony, separation en controller, recherche d'un terme

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'title' => 'Bienvenue']);
+    }
+}
--- src/lib/function/function.jdm_request.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function getCodeFor($mot){
-  $page = file_get_contents("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=".$mot."&rel=");
-  return $page;
-}
-
-?>
