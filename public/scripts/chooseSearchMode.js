093565ddb5309c5f9d34a1caff0b65261ac92d25
Clément <DrakenXI@users.noreply.github.com>
2020-01-05 23:56:46 +0100

    display relation list

--- public/scripts/chooseSearchMode.js
+++ public/scripts/chooseSearchMode.js
@@ -48,12 +48,15 @@ function setExplanationText() {
     var form = document.getElementById('form');
     var mode = document.getElementById('search-mode');
     var text;
+    var relationList = document.getElementById('relations');
+    relationList.style.visibility = "hidden";
     if("exacte" == mode.value) {
         text = "Recherche le terme tel quel parmi les entrées de Jeux de Mots.";
     } else if ("approximative" == mode.value) {
         text = "Recherche le terme approximativement parmi les entrées de Jeux de Mots.";
     } else if ("relation" == mode.value) {
         text = "Recherche le terme associé à la relation choisie dans Jeux de Mots.";
+        relationList.style.visibility = "visible";
     } else if ("chaine" == mode.value){
         text = "Recherche la chaîne dans Jeux de Mots.";
     } else {
