bbf53609d3cb53b165f92b8ea1e9250b5f0bebea
Amandine Paillard <amandine.paillard@hotmail.fr>
2020-01-05 23:01:42 +0100

    premiere page fonctionnelle et routes ok

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,10 +9,13 @@ class BlogController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     * TODO relations
      */
     public function index()
     {
+        $relations = ['toto', 'titi', 'tata', 'tutu', 'tete'];
         return $this->render('blog/index.html.twig', [
-            'title' => 'Bienvenue']);
+            'title' => 'Bienvenue',
+            'relations' => $relations]);
     }
 }
--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -13,9 +13,42 @@ class SearchController extends AbstractController
     }
 
     /**
-     * @Route("/search/{term}", name="search", requirements={"term"="[a-zA-Z0-9]+([a-zA-Z0-9]*)"})
+     * @Route("/search/{term}", name="search", requirements={"term"="[a-zA-Z0-9]+[a-zA-Z0-9]*"})
      */
     public function search(string $term)
+    {
+        return $this->render('search/index.html.twig', [
+            'title' => 'Affichage du mot ' . $term,
+            'content' => $this->getHtmlContentFor($term),
+        ]);
+    }
+
+    /**
+     * @Route("/search-approx/{term}", name="search-approx", requirements={"term"="[a-zA-Z0-9]+[a-zA-Z0-9]*"})
+     */
+    public function searchApprox(string $term)
+    {
+        return $this->render('search/index.html.twig', [
+            'title' => 'Affichage du mot ' . $term,
+            'content' => $this->getHtmlContentFor($term),
+        ]);
+    }
+
+    /**
+     * @Route("/search-relations/{term}/{relations}", name="search-relations", requirements={"term"="[a-zA-Z0-9]+[a-zA-Z0-9]*", "relations"="[a-zA-Z0-9]+[a-zA-Z0-9]*&[a-zA-Z0-9]+[a-zA-Z0-9]*"})
+     */
+    public function searchRelations(string $term, string $relations)
+    {
+        return $this->render('search/index.html.twig', [
+            'title' => 'Affichage du mot ' . $term,
+            'content' => $this->getHtmlContentFor($term),
+        ]);
+    }
+
+    /**
+     * @Route("/search-string/{term}", name="search-string", requirements={"term"="[a-zA-Z0-9]+[a-zA-Z0-9]*"})
+     */
+    public function searchString(string $term)
     {
         return $this->render('search/index.html.twig', [
             'title' => 'Affichage du mot ' . $term,
