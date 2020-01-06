750956bc29cf705164123f892a265dbc6b85b278
Clément <DrakenXI@users.noreply.github.com>
2020-01-06 17:48:02 +0100

    correction du code clean (encore quelque problèmes mais mieux)

--- src/Functions/CodeCleaner.php
+++ src/Functions/CodeCleaner.php
@@ -4,45 +4,45 @@ namespace App\Functions;
 
 class CodeCleaner
 {
-  public function cleanCode($page){
-    $start = stripos($page,'<code>');
-    $stop = stripos($page,'</code>');
-    //echo "Start : " . $start . " | Stop : " . $stop . "<br>";
-    if($start !== false && $stop !== false){
-      $code = substr($page, $start+6, $stop-$start-7);
-      $code = utf8_encode($code);
-      //echo $code;
-      $cleanData = new class{}; //Objet anonyme
-      $cleanData->defs = $this->getDefs($code);
+    public function cleanCode($page){
+        $start = stripos($page,'<code>');
+        $stop = stripos($page,'</code>');
+        //echo "Start : " . $start . " | Stop : " . $stop . "<br>";
+        if($start !== false && $stop !== false){
+            $code = substr($page, $start+6, $stop-$start-7);
+            $code = utf8_encode($code);
+            //echo $code;
+            $cleanData = new class{}; //Objet anonyme
+            $cleanData->defs = $this->getDefs($code);
 
-      $cleanData->datas = $this->getDatas($code);
+            $cleanData->datas = $this->getDatas($code);
 
-      return $cleanData;
-    }else{
-      //echo "code non trouver";
+            return $cleanData;
+        }else{
+            //echo "code non trouver";
+        }
     }
-  }
 
-  public function getDefs($code){
-    $start = stripos($code,'<def>');
-    $stop = stripos($code,'</def>');
-    if($start !== false && $stop !== false){
-      $defs = substr($code, $start+5, $stop-$start-7);
-      return preg_split("<br>",$defs);
+    public function getDefs($code){
+        $start = stripos($code,'<def>');
+        $stop = stripos($code,'</def>');
+        if($start !== false && $stop !== false){
+            $defs = substr($code, $start+5, $stop-$start-7);
+            return preg_split("<br>",$defs);
+        }
+        return "";
     }
-    return "";
-  }
 
-  public function getDatas($code){
-    $start = stripos($code,'</def>');
-    $result = array();
-    if($start !== false){
-      $datas = substr($code, $start+6);
-      foreach(preg_split("/\n|\r/",$datas) as $d){
-        array_push($result,(explode(";", $d)));
-      }
-      return $result;
+    public function getDatas($code){
+        $start = stripos($code,'</def>');
+        $result = array();
+        if($start !== false){
+            $datas = substr($code, $start+6);
+            foreach(preg_split("/\n|\r/",$datas) as $d){
+                array_push($result,(str_getcsv($d ,";" , "'")));
+            }
+            return $result;
+        }
+        return "";
     }
-    return "";
-  }
 }
--- src/Functions/JDMRequest.php
+++ src/Functions/JDMRequest.php
@@ -5,84 +5,91 @@ namespace App\Functions;
 class JDMRequest
 {
 
-  private $cleaner;
+    private $cleaner;
 
-  public function __construct()
-  {
-      $this->cleaner = new CodeCleaner();
-  }
-
-  function getCodeFor($mot)
-  {
-    //$wordCache = getCacheByWord($mot);
-
-    $wordCache = null;
-
-    //sinon on fait la requete et on netoye
-
-    $page = file_get_contents("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" . $mot . "&rel=");
-
-    $code = $this->cleaner->cleanCode($page);
-
-    $affichage = "<table><thead><tr><th>N°</th><th>Definition</th></tr></thead><tbody>";
-
-
-    for ($i = 1; $i < sizeof($code->defs); $i++) {
-      $affichage = $affichage . "<tr><td>" . $i . "</td><td>" . $code->defs[$i] . "</td></tr>";
+    public function __construct()
+    {
+        $this->cleaner = new CodeCleaner();
     }
-    $affichage = $affichage . "</tbody></table>";
 
-    $nodeType = array(); //Tableau associatif
-    $relationType = array();
-    $entries = array();
+    function getCodeFor($mot)
+    {
+        //$wordCache = getCacheByWord($mot);
 
-    $rt = 2; //type de relation 2: sortante 3:entrante
+        $wordCache = null;
 
-    for ($i = 1; $i < sizeof($code->datas); $i++) {
+        //sinon on fait la requete et on netoye
 
-      if (sizeof($code->datas[$i]) >= 2) {//Si c'est des data (supprime les \n)
+        $page = file_get_contents("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" . $mot . "&rel=");
 
-        switch ($code->datas[$i][0]) {
-          case 'nt': //node type
-            $nodeType["id_" . $code->datas[$i][1]] = $code->datas[$i];
-            break;
-          case 'rt': //relation type
-            $relationType["id_" . $code->datas[$i][1]] = $code->datas[$i];
-            break;
-          case 'e':// entries
-            $name = $code->datas[$i][2];
-            if (isset($code->datas[$i][5])) {
-              $code->datas[$i][5];
-            }
-            $affichage = $affichage . "<tr><td>" . $name . "</td><td>" . $nodeType["id_" . $code->datas[$i][3]][2] . "</td><td>" . $code->datas[$i][4] . "</td></tr>";
-            $entries["id_" . $code->datas[$i][3]] = $name; //on l'ajout dans la liste des entreis
-            break;
-          case 'r':// relation
-            //$affichage = $affichage . "<tr><td>" . $entries["id_" . $code->datas[$i][$rt]] . "</td><td>" . $relationType["id_" . $code->datas[$i][4]][2] . "</td><td>" . $code->datas[$i][5] . "</td></tr>";
-            break;
-          default:
-            if (strpos($code->datas[$i][0], "// les noeuds/termes (Entries) : e") !== false) {//entrer
-              $affichage = $affichage . "<table><thead><tr><th>Nom</th><th>Type de noeud</th><th>Poids</th></tr></thead><tbody>";
-            } elseif (strpos($code->datas[$i][0], "// les relations sortantes : r") !== false) {//relation sortant
-              $affichage = $affichage . "</tbody></table><table><thead><tr><th>Nom relation sortante</th><th>Type de relation</th><th>Poids</th></tr></thead><tbody>";
-            } elseif (strpos($code->datas[$i][0], "// les relations entrantes : r") !== false) {//relation sortant
-              $affichage = $affichage . "</tbody></table><table><thead><tr><th>Nom relation entrantre</th><th>Type de relation</th><th>Poids</th></tr></thead><tbody>";
-              $rt = 3;
-            }
-            break;
+        $code = $this->cleaner->cleanCode($page);
+
+        $affichage = "<table><thead><tr><th>N°</th><th>Definition</th></tr></thead><tbody>";
+
+
+        for ($i = 1; $i < sizeof($code->defs); $i++) {
+            $affichage = $affichage . "<tr><td>" . $i . "</td><td>" . $code->defs[$i] . "</td></tr>";
         }
+        $affichage = $affichage . "</tbody></table>";
 
-      }
+        $nodeType = array(); //Tableau associatif
+        $relationType = array();
+        $entries = array();
 
-    }//end for
+        $rt = 2; //type de relation 2: sortante 3:entrante
 
-    $affichage = $affichage . "</tbody></table>";
+        for ($i = 1; $i < sizeof($code->datas); $i++) {
 
-    /*echo $affichage;
+            if (sizeof($code->datas[$i]) >= 2) {//Si c'est des data (supprime les \n)
 
-    echo "Node type";
-    print_r($nodeType);*/
+                switch ($code->datas[$i][0]) {
+                    case 'nt': //node type
+                        $nodeType["id_" . $code->datas[$i][1]] = $code->datas[$i];
+                        break;
+                    case 'rt': //relation type
+                        $relationType["id_" . $code->datas[$i][1]] = $code->datas[$i];
+                        break;
+                    case 'e':// entries
 
-    return $affichage;
-  }
+                        $name = $code->datas[$i][2];
+
+                        if (isset($code->datas[$i][5])) {
+                            $code->datas[$i][5];
+                        }
+
+                        $affichage = $affichage . "<tr><td>" . $name . "</td><td>" . $nodeType["id_" . $code->datas[$i][3]][2] . "</td><td>" . $code->datas[$i][4] . "</td></tr>";
+                        $entries["id_" . $code->datas[$i][3]] = $name; //on l'ajout dans la liste des entreis
+                        break;
+                    case 'r':// relation
+                        $ent = "EMPTY";
+                        if(isset($entries["id_" . $code->datas[$i][3]])){
+                            $ent = $entries["id_" . $code->datas[$i][3]];
+                        }
+                        $affichage = $affichage . "<tr><td>" . $ent . "</td><td>" . $relationType["id_" . $code->datas[$i][4]][3] . "</td><td>" . $code->datas[$i][5] . "</td></tr>";
+                        break;
+                    default:
+                        if (strpos($code->datas[$i][0], "// les noeuds/termes (Entries) : e") !== false) {//entrer
+                            $affichage = $affichage . "<table><thead><tr><th>Nom</th><th>Type de noeud</th><th>Poids</th></tr></thead><tbody>";
+                        } elseif (strpos($code->datas[$i][0], "// les relations sortantes : r") !== false) {//relation sortant
+                            $affichage = $affichage . "</tbody></table><table><thead><tr><th>Nom relation sortante</th><th>Type de relation</th><th>Poids</th></tr></thead><tbody>";
+                        } elseif (strpos($code->datas[$i][0], "// les relations entrantes : r") !== false) {//relation entrantes
+                            $affichage = $affichage . "</tbody></table><table><thead><tr><th>Nom relation entrantre</th><th>Type de relation</th><th>Poids</th></tr></thead><tbody>";
+                            $rt = 3;
+                        }
+                        break;
+                }
+
+            }
+
+        }//end for
+
+        $affichage = $affichage . "</tbody></table>";
+
+        /*echo $affichage;
+
+        echo "Node type";
+        print_r($nodeType);*/
+
+        return $affichage;
+    }
 }
