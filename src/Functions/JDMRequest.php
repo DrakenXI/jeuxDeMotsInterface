e3a9acc90ed7d2d7240a7e26c55f5b50198dcd6e
Clément <DrakenXI@users.noreply.github.com>
2020-01-15 19:35:11 +0100

    cache corriger

--- src/Functions/JDMRequest.php
+++ src/Functions/JDMRequest.php
@@ -2,35 +2,40 @@
 
 namespace App\Functions;
 
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
 class JDMRequest
 {
 
     private $cleaner;
+    private $cache;
 
     private $cacheDuraction;
 
     public function __construct()
     {
         $this->cleaner = new CodeCleaner();
+        $this->cache = new FilesystemAdapter();
         $this->cacheDuraction = 604800;
     }
 
     /**
      * Main research mode
      */
-    function getDataFor($mot)
+    function getDataFor($term)
     {
         //$wordCache = getCacheByWord($mot);
         $wordCache = null;
 
-        $mot = convertToAnsi($mot);
+        $term = convertToAnsi($term);
 
         //sinon on fait la requete et on nettoie
 
-        $nomCache = 'cache-req-exacte-'.$mot;
-        $page = $this->cache->get($nomCache, function (ItemInterface $item) use ($mot) {
+        $nomCache = 'cache-req-exacte-'.$term;
+        $page = $this->cache->get($nomCache, function (ItemInterface $item) use ($term) {
             $item->expiresAfter($this->cacheDuraction);
-            $page = file_get_contents("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" . $mot . "&rel=");
+            $page = file_get_contents("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" . $term . "&rel=");
             return $page;
         });
 
@@ -49,8 +54,12 @@ class JDMRequest
         //$wordCache = getCacheByWord($mot);
         $wordCache = null;
 
-        //sinon on fait la requete et on nettoie
-        $page = file_get_contents("http://www.jeuxdemots.org/autocompletion/autocompletion.php?completionarg=proposition&proposition=".convertToAnsi($term)."t&trim=1");
+        $nomCache = 'cache-req-approx-'.$term;
+        $page = $this->cache->get($nomCache, function (ItemInterface $item) use ($term) {
+            $item->expiresAfter($this->cacheDuraction);
+            $page = file_get_contents("http://www.jeuxdemots.org/autocompletion/autocompletion.php?completionarg=proposition&proposition=".convertToAnsi($term)."t&trim=1");
+            return $page;
+        });
 
         $terms = array();
         foreach(preg_split("/ \* /",utf8_encode($page)) as $str){
@@ -66,8 +75,30 @@ class JDMRequest
         //$wordCache = getCacheByWord($mot);
         $wordCache = null;
 
-        $termId = $this->cleaner->getTermId(file_get_contents("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" .convertToAnsi($term) . "&rel="))[0];
-        $page = file_get_contents("http://www.jeuxdemots.org/diko.php?select_relation_type=".$relation."&gotermrel_id=".$termId);
+        $term = convertToAnsi($term);
+
+        $nomCache = 'cache-req-termId-'.$term;
+        $termId = $this->cache->get($nomCache, function (ItemInterface $item) use ($term) {
+            $item->expiresAfter($this->cacheDuraction);
+            $termId = $this->cleaner->getTermId(file_get_contents("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" . $term . "&rel="))[0];
+            return $termId;
+        });
+
+        $nomCache = 'cache-req-relation-'.$termId.'-'.$relation;
+        $page = $this->cache->get($nomCache, function (ItemInterface $item) use ($relation,$termId) {
+            $item->expiresAfter($this->cacheDuraction);
+            $page = file_get_contents("http://www.jeuxdemots.org/diko.php?select_relation_type=".$relation."&gotermrel_id=".$termId);
+            return $page;
+        });
+
+
+        $nomCache = 'cache-clean-relation-'.$termId.'-'.$relation;
+        $page = $this->cache->get($nomCache, function (ItemInterface $item) use ($relation,$termId) {
+            $item->expiresAfter($this->cacheDuraction);
+            $page = file_get_contents("http://www.jeuxdemots.org/diko.php?select_relation_type=".$relation."&gotermrel_id=".$termId);
+            return $page;
+        });
+
         $response = $this->cleaner->getEntriesForRelation($page);
         return $response;
     }
